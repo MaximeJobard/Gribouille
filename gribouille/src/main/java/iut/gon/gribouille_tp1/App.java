7d7cfe8e414a0f602a6a3c093dae5de31caf893d
Maxime <maxime.jobard@etu.unicaen.fr>
2023-04-06 14:49:35 +0200

    dessin_VF

--- gribouille/src/main/java/iut/gon/gribouille_tp1/App.java
+++ gribouille/src/main/java/iut/gon/gribouille_tp1/App.java
@@ -5,6 +5,11 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,10 +20,12 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    double X;
+    double Y;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("CadreGribouille"), 1920, 1080);
+        scene = new Scene(loadFXML("CadreGribouille"), 960, 540);
         stage.setScene(scene);
         stage.show();
         stage.setOnCloseRequest(event -> {
@@ -30,7 +37,34 @@ public class App extends Application {
         }
         }
         );
-//        sdfsd
+        
+        Canvas dessin = (Canvas) scene.lookup("Canvas");
+        
+//        Pane pane = (Pane) dessin.getParent();
+//        
+//        pane.addEventFilter(MouseEvent.MOUSE_PRESSED, (evt) -> {
+//                if(evt.getButton() == MouseButton.SECONDARY) {
+//                        Circle circle =new Circle();
+//                        circle.setRadius(5.0);
+//                X = evt.getX(); Y = evt.getY();
+//                circle.setCenterX(X);
+//                circle.setCenterY(Y);
+//                
+//                dessin.addEventHandler(MouseEvent.MOUSE_PRESSED, (event) -> {X = evt.getX(); Y = evt.getY();});
+//                pane.getChildren().add(circle);
+//                
+//                }
+        
+        dessin.addEventHandler(MouseEvent.MOUSE_PRESSED, (evt) -> {X = evt.getX(); Y = evt.getY();});
+        dessin.addEventHandler(MouseEvent.MOUSE_DRAGGED, (evt) -> {dessin.getGraphicsContext2D().strokeLine(X, Y, evt.getX(), evt.getY());
+        this.X = evt.getX();
+        this.Y = evt.getY();
+        });
+//        });
+        
+        
+        
+        
         
     }
 
--- gribouille/src/main/java/module-info.java
+++ gribouille/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module iut.gon.gribouille_tp1 {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
+	requires java.desktop;
+	requires java.xml;
 
     opens iut.gon.gribouille_tp1 to javafx.fxml;
     exports iut.gon.gribouille_tp1;
