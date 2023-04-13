3b2246304ed339fe0a15d7007a8a48ac3979b70a
Maxime <maxime.jobard@etu.unicaen.fr>
2023-04-13 11:49:10 +0200

    contenue_des_labels

--- tp3/src/main/java/iut/gon/tp3/GrilleController.java
+++ tp3/src/main/java/iut/gon/tp3/GrilleController.java
@@ -5,14 +5,36 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.TextAlignment;
 
 public class GrilleController implements Initializable {
 	@FXML GridPane grille;
+	Label[][] tab;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		grille = new GridPane();
-		grille.setStyle("-fx-background-color: seashell");		
+		grille.setStyle("-fx-background-color: seashell");	
+
+		tab = new Label[3][3];
+		for(int i = 0 ; i < 3 ; i++) {
+			for(int j = 0 ; j < 3 ; j++) {
+				Label label = new Label("azeqsd"); 
+				label.setMaxSize(1000, 1000);
+				label.setAlignment(Pos.CENTER);
+
+				tab[i][j] = label;
+				// Un seul necessaire
+				tab[i][j].addEventHandler(MouseEvent.MOUSE_CLICKED, (evt) -> {label.setText("Bonjour");});
+				tab[i][j].setOnMouseClicked((evt)-> {label.setText("Bonjour");});
+				
+				grille.add(tab[i][j], i, j);
+
+			}	
+		}
+
 	}
 }
