e4c8d13f88ad57fcc359a43bf9e3d3a80c87d2a9
Maxime <maxime.jobard@etu.unicaen.fr>
2023-05-09 17:06:57 +0200

    a finir

--- tp5/tp5/src/main/java/fr/unicaen/iut/tp5/ControllerDemineur.java
+++ tp5/tp5/src/main/java/fr/unicaen/iut/tp5/ControllerDemineur.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
@@ -14,12 +15,18 @@ import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.TextAlignment;
 
 public class ControllerDemineur implements Initializable {
 
@@ -86,17 +93,29 @@ public class ControllerDemineur implements Initializable {
 		});
 	}
 
+	BackgroundFill aqua = new BackgroundFill(Color.AQUA, new CornerRadii(0.2), new Insets(0));
+	BackgroundFill lightgray = new BackgroundFill(Color.LIGHTGRAY, new CornerRadii(0), new Insets(0));
+	BackgroundFill red = new BackgroundFill(Color.RED, new CornerRadii(0.2), new Insets(0));
+	BackgroundFill lemonchiffon = new BackgroundFill(Color.LEMONCHIFFON, new CornerRadii(0.2), new Insets(0));
+
+	Background inconnu = new Background(aqua);
+	Background libre = new Background(lightgray);
+	Background echec = new Background(red);
+	Background marquee = new Background(lemonchiffon);
+
 	public void initGrille(String s) {
 		gridpane.getColumnConstraints().clear();
 		gridpane.getRowConstraints().clear();
 		int[] t = modele.parseUserData(s);
 		modele.setTaille(t[0], t[1], t[2]);
 
-		for(int i = 0; i < t[0]; i++){
-			
-				gridpane.getColumnConstraints().add(new ColumnConstraints(32));
-				gridpane.getRowConstraints().add(new RowConstraints(32));
-			
+		for (int i = 0; i < t[0]; i++) {
+			gridpane.getColumnConstraints().add(new ColumnConstraints(32));
+			gridpane.getRowConstraints().add(new RowConstraints(32));
+			Label l = new Label("?");
+			l.setBackground(inconnu);
+			l.setPrefSize(31, 31);
+			l.setTextAlignment(TextAlignment.CENTER);
 		}
 	}
 }
